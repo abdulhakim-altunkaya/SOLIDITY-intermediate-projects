142c074a4726594ad1d462607e1655d154f3b6aa
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-06-18 14:25:32 +0300

    second token project is mostly finished. js files modified, env file created and used in hardhat.config.js. Alchemy link obtained.

--- 5_token/contracts/SpindelToken.sol
+++ 5_token/contracts/SpindelToken.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SpindelToken is ERC20 {
 
-    constructor() ERC20("SpindelToken", "SPDL"){
-        _mint(msg.sender, 5000*1000**18);
+    constructor(uint _initialAmount) ERC20("SpindelToken", "SPDL"){
+        _mint(msg.sender, _initialAmount);
     }
 
 
--- 5_token/scripts/deploy.js
+++ 5_token/scripts/deploy.js
@@ -6,20 +6,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const SpindelToken = await hre.ethers.getContractFactory("DevToken");
+  const spindelToken = await SpindelToken.deploy("1000000000000000000000000000");
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await spindelToken.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", spindelToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
