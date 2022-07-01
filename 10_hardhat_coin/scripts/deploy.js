ff3176349fb6ad7d7359263fb674f34339282d5d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-01 21:15:01 +0300

    hardhat coin deploy.js finalized. second project of hardhat coin project started this time I will store coins in a different. I mean msg.sender and account having coins wwill be different. Then I will try to do a similar project on truffle

--- 10_hardhat_coin/scripts/deploy.js
+++ 10_hardhat_coin/scripts/deploy.js
@@ -3,9 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const MyraCoin = await hre.ethers.getContractFactory("MyraCoin");
   const myraCoin = await MyraCoin.deploy();
-
   await myraCoin.deployed();
-
   console.log("Token deployed to:", myraCoin.address);
 }
 
