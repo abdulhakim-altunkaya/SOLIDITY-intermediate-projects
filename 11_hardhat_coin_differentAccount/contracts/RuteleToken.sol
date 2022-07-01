c78f0a30eb81c1052a50fc4704d786a9dbeb4b1f
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-01 21:49:17 +0300

    project finished. accounts were separated. msg.sender and coin owner are two accounts.

---
+++ 11_hardhat_coin_differentAccount/contracts/RuteleToken.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract RuteleToken is ERC20 {
+
+    constructor() ERC20("RuteleToken", "RSAL"){
+
+        _mint(0x8f2541f8372220612121baAb879c6184428de532, 500*10**18);
+
+    }
+}
