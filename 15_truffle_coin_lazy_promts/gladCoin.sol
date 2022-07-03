739b6870206bafc7c00e26cc6fb067495a31e496
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-03 14:47:59 +0300

    project deployed but metamask prompt is still the same. This time will expand it with _approve approve allowance transferFrom

---
+++ 15_truffle_coin_lazy_promts/gladCoin.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+//LAZY UNCAPPED SUPPLY
+contract FleraCoin is ERC20 {
+    constructor() ERC20("FleraCoin", "FLERA") {}
+
+    function mintTokens() external {
+        _mint(msg.sender, 10*10**18);
+    }
+    // spender and recipient : contract
+    // owner and sender :  msg.sender
+    function promtAccess1(address spender, uint amount) external {
+        approve(spender, amount);
+    }
+
+    function promtAccess2(address spender, uint amount) external {
+        approve(spender, amount);
+        _approve(msg.sender, spender, amount);
+    }
+
+    function promtAccess3(address spender, uint amount) external {
+        _approve(msg.sender, spender, amount);
+    }
+
+    function promtAccess4(address spender) external {
+        allowance(msg.sender, spender);
+    }
+
+    function promtAccess5(address recipient, uint amount) external {
+        transferFrom(msg.sender, recipient, amount);
+    }
+
+}
+
