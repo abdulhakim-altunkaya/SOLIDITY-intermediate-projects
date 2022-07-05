20d8108d237be494cb2bc84d232d72a41f85502b
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-05 17:23:17 +0300

    react part is mostly finished.

--- 18_/src/App.js
+++ 18_/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 
-function App {
+
+function App() {
+  const connectMetamask = () => {
+    return;
+  }
+  const connectContract = () => {
+    return;
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={}=></button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={connectMetamask}>CONNECT TO METAMASK</button>
+        <p id="areaMetamask"></p>
+        <button onClick={connectContract}>CONNECT TO CONTRACT</button>
+        <p id="areaConnection"></p>
       </header>
     </div>
   );
@@ -24,11 +21,3 @@ function App {
 
 export default App;
 
-
-<div id="readArea">
-  <button onClick="connectMetamask">CONNECT TO METAMASK</button>
-  <p id="userArea">Status: Not connected to Metamask</p>
-  <button onclick="connectContract">CONNECT TO CONTRACT</button>
-  <p id="contractArea">Status: Not connected to Contract</p>
-  <button onclick="mintTokens2">MINT TOKENS</button>
-</div>
