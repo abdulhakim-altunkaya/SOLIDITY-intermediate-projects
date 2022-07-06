6b0ef5891418594d590b47433268c0ec163f4599
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-06 20:56:07 +0300

    truffle project created after finishing react

---
+++ 18_/contracts/Test.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.8.4;
+
+contract Test {
+    string public myName = "liutas";
+
+    function changeName(string memory _name) external {
+        myName = _name;
+    }
+}
