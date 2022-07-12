a50ddb1e268a8758c3e1da5e7ce5c24e9c9330f5
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-12 14:27:32 +0300

    react folder cleaned up. metamask connection is done

--- 16_raect_hardhat_lazycap/src/App.js
+++ 16_raect_hardhat_lazycap/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const { ethereum } = window;
+  let [account, setAccount] = useState("");
+  let [contractData, setContractData] = useState("");
+  let [coinBalance, setCoinBalance] = useState("");
+
+  const connectMetamask = async () => {
+    if(window.ethereum !== "undefined") {
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      setAccount(accounts[0]);
+    }
+  }
+
+  const connectContract = async () => {
+    return;
+  }
+  const getData = async () => {
+    return;
+  }
+
+  const getCoins = async () => {
+    return;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <button onClick={connectMetamask}>CONNECT TO METAMASK</button>
+          <p>{account}</p>
+          <button onClick={connectContract}>CONNECT TO CONTRACT</button> <br /><br />
+          <button onClick={getData}>READ FROM CONTRACT</button>
+          <p>{contractData}</p>
+          <button onClick={getCoins}>GET 10 COINS</button>
+          <p>{coinBalance}</p>
     </div>
   );
 }
-
 export default App;
