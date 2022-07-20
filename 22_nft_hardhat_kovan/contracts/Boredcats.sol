16ee33591e617a2b9da944ea9aab1f1e9c356f95
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-20 14:05:00 +0300

    contract side is finished. contract is uploaded to goerli scan for public verification. Alchemy+.env part finished. Only nft upload part is left.

---
+++ 22_nft_hardhat_kovan/contracts/Boredcats.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Boredcats is ERC721, ERC721URIStorage {
+
+    //Here we are creating a counters library and creating a counters variable.
+    //_tokenIdCounter variable will keep track of token id.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+
+    constructor() ERC721("Boredcats", "BC") {}
+
+    //First parameter is the recipient of the newly minted NFT.
+    //Second parameter is uri which will describe the metadata of the NFT.
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current(); // We are storing the current token id.
+        _tokenIdCounter.increment();    //increment the id number by one.
+        _safeMint(to, tokenId); // mint the nft. This is the main function.
+        _setTokenURI(tokenId, uri); // after minting it, set the metadata to this token id.
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
