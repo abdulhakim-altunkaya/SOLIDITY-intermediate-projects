65178a429d784ab285aee82bf81e26f472e10bd4
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-26 22:47:48 +0300

    nft contracy has been improved with require statements and tested on remix. Next is to add uri property to the contract and start minting image nfts

--- 26_nft_hardhat_polygon_multiple_4/contracts/Nusaybin.sol
+++ 26_nft_hardhat_polygon_multiple_4/contracts/Nusaybin.sol
@@ -1,37 +1,43 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Nusaybin is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract Nusaybin is ERC721, Ownable {
+    uint256 public mintPrice = 0.05 ether;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    bool public isMintEnabled;
 
-    Counters.Counter private _tokenIdCounter;
+    //the number of nft each wallet has
+    //Also by using this mapping, we can prevent a single wallet 
+    //to mint all nft. We can set a limit.
+    mapping(address => uint256) public mintedWallets;
 
-    constructor() ERC721("Nusaybin", "NSBN") {}
-
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    constructor() payable ERC721("Nusaybin", "NSBN") {
+        maxSupply = 2;
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+    function toggleIsMintEnabled() external onlyOwner {
+        isMintEnabled = !isMintEnabled;
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+    function setMaxSupply(uint256 _maxSupply) external onlyOwner {
+        maxSupply = _maxSupply;
+    }
+
+    function mint() external payable {
+        require(isMintEnabled, "minting not enabled");
+        require(mintedWallets[msg.sender] < 1, "exceeds max quota per wallet");
+        require(msg.value == mintPrice, "wrong value");
+        require(maxSupply > totalSupply, "sold out");
+
+        mintedWallets[msg.sender]++;
+        totalSupply++;
+        uint256 tokenId = totalSupply; // As we have set tokenId to 1 (by saying totalSupply++; in the line above), token ids will start from 1 not from 0.
+
+        _safeMint(msg.sender, tokenId);
     }
 }
