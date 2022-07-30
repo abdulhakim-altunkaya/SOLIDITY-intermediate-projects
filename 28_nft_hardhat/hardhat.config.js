461bb463dc5f93d631fa58214427f922aa0ff121
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-30 13:53:56 +0300

    standard nft contract improved with minting price and burn function

--- 28_nft_hardhat/hardhat.config.js
+++ 28_nft_hardhat/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.1",
+  networks: {
+    matic: {
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
