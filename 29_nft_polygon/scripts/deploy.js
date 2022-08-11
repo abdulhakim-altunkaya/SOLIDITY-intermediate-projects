dd94deaefc2bb2d624d3adada0598f3524d948b7
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-08-11 23:51:44 +0300

    hardhat config settings and deploy file settings are done. Cizre contract version made compatible with hardhat config and other contract files

--- 29_nft_polygon/scripts/deploy.js
+++ 29_nft_polygon/scripts/deploy.js
@@ -7,18 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Cizre = await hre.ethers.getContractFactory("Cizre");
+  const cizre = await Cizre.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await cizre.deployed();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("cizre deployed to:", cizre.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
