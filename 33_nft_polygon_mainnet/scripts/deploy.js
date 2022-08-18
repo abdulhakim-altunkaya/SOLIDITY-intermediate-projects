0b7a33dae0650c3a57d580863c76c6829d59e66e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-08-19 00:25:35 +0300

    all configuration is done and contract is successfully deployed to polygon matic mainnet. next steps are flattening the contract and verifying it on polygon scan. later starting to mint with new json format. for image hosting I will be going with IPFS

--- 33_nft_polygon_mainnet/scripts/deploy.js
+++ 33_nft_polygon_mainnet/scripts/deploy.js
@@ -7,20 +7,10 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const Nineteenstars = await hre.ethers.getContractFactory("Nineteenstars");
+  const nineteenstars = await Nineteenstars.deploy();
+  await nineteenstars.deployed();
+  console.log(`Nineteenstars deployed to ${nineteenstars.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
