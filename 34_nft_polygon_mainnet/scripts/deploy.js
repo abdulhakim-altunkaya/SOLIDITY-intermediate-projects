53d3054bfb3f2d9904c5d833795d0bf9ae5ae907
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-08-21 17:55:10 +0300

    contract is deployed to mainnet, configuration is done

--- 34_nft_polygon_mainnet/scripts/deploy.js
+++ 34_nft_polygon_mainnet/scripts/deploy.js
@@ -7,20 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Seventeenstars = await hre.ethers.getContractFactory("Seventeenstars");
+  const seventeenstars = await Seventeenstars.deploy();
 
-  await lock.deployed();
+  await seventeenstars.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`seventeenstars is deployed to ${seventeenstars.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
