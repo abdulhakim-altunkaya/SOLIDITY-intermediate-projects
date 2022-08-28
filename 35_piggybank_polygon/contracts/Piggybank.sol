ffaba0804589a3e4980e07ebe1117f442dbcef64
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-08-28 19:37:53 +0300

    contract is finished with event emitters and selfdestruct function

--- 35_piggybank_polygon/contracts/Piggybank.sol
+++ 35_piggybank_polygon/contracts/Piggybank.sol
@@ -1,23 +1,36 @@
 //SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.8.1;
+
 contract PiggyBank {
+    event Deposit(address sender, uint amount);
+    event Withdraw(address receiver, uint amount);
+    event Destroy(uint amount, string text);
+
     address payable public owner;
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    modifier onlyOwner(){
+    modifier onlyOwner() {
         require(msg.sender == owner, "you are not the owner");
+        _;
     }
 
     function depositEther() external payable {
-
+        emit Deposit(msg.sender, msg.value);
     }
 
     function withdrawEther(address payable _to, uint _amount) external onlyOwner {
         (bool success, ) = _to.call{value: _amount}("");
         require(success, "probably no money in contract");
+        emit Withdraw(_to, _amount);
+    }
+
+    function destroyContract(address _to) external onlyOwner{
+        selfdestruct(payable(_to));
+        emit Destroy(address(this).balance, "contract is destroyed");
     }
 
     function getBalance() external view returns(uint) {
