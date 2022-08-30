48faf2b72dccd650ccb214c58a35398f80f1da5d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-08-30 15:09:16 +0300

    deposit component is finished. Instead of using function parameters, I used another way to send value to the contract.

--- 35_piggybank_polygon/src/components/Deposit.js
+++ 35_piggybank_polygon/src/components/Deposit.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {ABI} from "./ContractABI.js";
+import { ethers } from "ethers";
+import { useState } from 'react';
 
 function Deposit() {
+  let[inputValue, setInputValue] = useState("");
+  let signer;
+  let contract;
+  const ABI_CONTRACT = ABI;
+
+  const connectContract = async () => {
+    const Address = "0xB5eF00143c460760e02Fd307fba828892A58c88C";
+    const ABI = ABI_CONTRACT;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+    contract = new ethers.Contract(Address, ABI, signer);
+  }
+  const depositMatic = async () => {
+    connectContract();
+    const txResponse = await contract.depositEther({value: inputValue});
+    await txResponse.wait();
+  }
   return (
     <div>
-      <button className='button-56'>Deposit Matic in Piggybank</button> <br />
-      <input type="number" placeholder='deposit...' /> 
+      <button className='button-56' onClick={depositMatic}>Deposit Matic in Piggybank</button> <br />
+      <input value={inputValue} type="number" placeholder='deposit...' onChange={e => setInputValue(e.target.value)} /> 
     </div>
   )
 }
 
-export default Deposit
+export default Deposit;
--- 35_piggybank_polygon/src/components/Withdraw.js
+++ 35_piggybank_polygon/src/components/Withdraw.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { ethers } from "ethers";
+import { ABI } from "./ContractABI.js";
 
 function Withdraw() {
+  let[inputValue, setInputValue] = useState("");
+  let provider;
+  let signer;
+  let contract;
+  const CONTRACT_ABI = ABI;
+
+  const connectContract = async () => {
+    const Address = "0xB5eF00143c460760e02Fd307fba828892A58c88C";
+    const ABI = CONTRACT_ABI;
+
+  }
+
+  const withdrawEther = async () => {
+    connectContract();
+  }
   return (
     <div>
         <br />
-        <button className='button-56' id="redButton" >Withdraw Matic (only Owner can click)</button> <br />
-        <input type="number" placeholder='withdraw...' /> 
+        <button className='button-56' id="redButton" onClick={withdrawEther()}>Withdraw Matic (only Owner can click)</button> <br />
+        <input value={inputValue} onChange={e => setInputValue(e.target.value)} type="number" placeholder='withdraw...' /> 
+        <p>{inputValue}</p>
     </div>
   )
 }
