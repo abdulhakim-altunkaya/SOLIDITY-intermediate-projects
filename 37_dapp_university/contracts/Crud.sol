6b1b5575cf9e361e8da971778fd5cbfcab5ef14e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-09-06 11:03:52 +0300

    crud contract contracts created with simple functionalities

---
+++ 37_dapp_university/contracts/Crud.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.7;
+
+contract Crud {
+    //CONFLICT OF VALUES: when you deploy contract,constructor values will win.
+    //Which means existing values of variables doesnt have effect. 
+
+    string public name = "my counter";
+    uint public count = 1;
+    constructor() {
+        name = "bla bla";
+        count = 2;
+    }
+}
+
+contract Crud2 {
+    string public name2;
+    uint public number2;
+    constructor(string memory _name) {
+        name2 = _name;
+    }
+
+    function increment(uint _number) external {
+        number2 = number2 + _number;
+    }
+}
