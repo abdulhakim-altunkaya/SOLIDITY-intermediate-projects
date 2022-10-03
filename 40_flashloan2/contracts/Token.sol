d66a164066a2fd2ace2ded1b4f8d578ff67c48e9
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-03 09:59:40 +0300

    spdx error fix attempt: all spdx versions unified, inherited contracts downloaded on local folder

--- 40_flashloan2/contracts/Token.sol
+++ 40_flashloan2/contracts/Token.sol
@@ -2,8 +2,8 @@
 
 pragma solidity >= 0.8.1;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "./ERC20.sol";
+import "./ERC20Capped.sol";
 
 contract Token is ERC20Capped {
     address public owner;
