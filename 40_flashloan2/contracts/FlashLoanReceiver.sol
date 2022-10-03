b02b5b012dfd6dd44af690fa4a13ef61548566ad
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-03 11:30:22 +0300

    flashloanreceiver contract is finished but further study is needed

--- 40_flashloan2/contracts/FlashLoanReceiver.sol
+++ 40_flashloan2/contracts/FlashLoanReceiver.sol
@@ -2,6 +2,34 @@
 
 pragma solidity >= 0.8.1;
 
+import "./FlashLoan.sol";
+
 contract FlashLoanReceiver {
-    
+    FlashLoan public pool;
+
+    address public owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "you are not owner");
+        _;
+    }
+
+    event LoanReceived(address token, uint amount);
+
+    constructor() {
+        pool = FlashLoan(0x8EEdA66fAC8e128f03e01C7511070C92402Ad254);
+        owner = msg.sender;
+    }
+
+    function receiveTokens(address _tokenAddress, uint _amount) external {
+        require(msg.sender == address(pool), "only pool can call this function");
+        require(Token(_tokenAddress).balanceOf(address(this)) == _amount, "receive xxx");
+        emit LoanReceived(_tokenAddress, _amount);
+        //do stuff with the money
+        //return funds to the pool
+        require(Token(_tokenAddress).transfer(msg.sender, _amount), "transfer funds back");
+    }
+
+    function executeFlashLoan(uint _amount) external onlyOwner {
+        pool.flashLoan(_amount);
+    }
 }
