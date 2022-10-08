cc966d43963201181135aeefc7467457835a3ca6
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-08 16:27:46 +0300

    contract created, dependencies isntalled.

---
+++ 44_single_swap/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.7.6",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/4X4MaNu5qcNpwsNw-ryjGgRmrCuH0tpL"
+      }
+    }
+  }
+};
