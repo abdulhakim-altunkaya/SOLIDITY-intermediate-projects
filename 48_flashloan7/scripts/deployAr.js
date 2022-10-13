45a4e13e8c3f6e2fbf353eec03fa0abce6fec56e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-13 19:24:51 +0300

    second aave flashloan example initiated

---
+++ 48_flashloan7/scripts/deployAr.js
@@ -0,0 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const FlashLoanArbitrage = await hre.ethers.getContractFactory("FlashLoanArbitrage");
+  const flashLoanArbitrage = await FlashLoanArbitrage.deploy("0xc4dCB5126a3AfEd129BC3668Ea19285A9f56D15D");
+  await flashLoanArbitrage.deployed();
+  console.log(`it is deployed to ${flashLoanArbitrage.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+    
