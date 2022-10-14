2440dee1601700c0092b3ee0e021c78064cf167d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-14 23:27:37 +0300

    test deployment was succesful. now flashloan contract is created to test flashloan without arbitrage on fantom network

---
+++ 50_flashloan_mumbai/contracts/Flashloan.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract Sample is FlashLoanSimpleReceiverBase {
+    address payable public owner;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "you are not owner");
+        _;
+    }
+    constructor(address _addressProvider) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
+        owner = msg.sender;
+    }
+
+    function executeOperation(
+        address asset,
+        uint amount,
+        uint fee,
+        address initiator,
+        bytes calldata params
+    ) external override returns(bool) {
+        uint amountOwed = amount + fee;
+        IERC20(asset).approve(address(POOL), amountOwed);
+        return true;
+    }
+
+    function requestFlashloan(address _token, uint _amount) public {
+        address receiverAddress = address(this);
+        address asset = _token;
+        uint amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
+        POOL.flashLoanSimple(receiverAddress, asset, amount, params, referralCode);
+    }
+
+    function getBalance(address _tokenAddress) external view returns(uint) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function withdraw(address _tokenAddress) external onlyOwner {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+    receive() external payable {}
+
+}
