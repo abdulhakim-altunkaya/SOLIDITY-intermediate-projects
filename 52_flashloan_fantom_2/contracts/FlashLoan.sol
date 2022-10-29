48d6e036a64e993f61327f67bca97acac0356ab7
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-29 16:25:51 +0300

    contract finished, ready for deployment and testing

---
+++ 52_flashloan_fantom_2/contracts/FlashLoan.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.10;
+
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract FlashLoan is FlashLoanSimpleReceiverBase {
+    address payable public owner;
+    error NotOwner(string message, address caller);
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert NotOwner("not owner", msg.sender);
+        }
+        _;
+    }
+
+    constructor(address _addressProvider) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
+        owner = payable(msg.sender);
+    }
+
+    function executeOperation(
+        address asset,
+        uint amount,
+        uint premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns(bool) {
+        uint amountOwed = amount + premium;
+        IERC20(asset).approve(address(POOL), amountOwed);
+        //approve(spender, amount)
+        return true;
+    }
+
+    function requestFlashLoan(address _token, uint _amount) public {
+        address receiverAddress = address(this);
+        address asset = _token;
+        uint amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
+        POOL.flashLoanSimple(receiverAddress, asset, amount, params, referralCode);
+    }
+
+    function getBalance(address _tokenAddress) external view returns(uint) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function withdraw(address _tokenAddress, uint _amount) external {
+        IERC20 tokkie = IERC20(_tokenAddress);
+        tokkie.transfer(msg.sender, _amount);
+    }
+
+    receive() external payable{}
+ }
