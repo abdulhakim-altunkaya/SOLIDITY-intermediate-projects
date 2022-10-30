c583b3ca01fe02316b8f76d1330cab652feead5f
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-10-30 18:53:20 +0300

    chainlink random number exercise finished

--- 52_flashloan_fantom_2/hardhat.config.js
+++ 52_flashloan_fantom_2/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.10",
   networks: {
-    polygon: {
+    fantom: {
       url: process.env.PROVIDER_URL,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
