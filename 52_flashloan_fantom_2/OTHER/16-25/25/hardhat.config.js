1a375926561fa600c5aff4056be655f19ea80b64
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-11-01 22:15:58 +0300

    flashloan exercise finished and archibveeed, now single swap uniswap exercise created

--- 52_flashloan_fantom_2/OTHER/16-25/25/hardhat.config.js
+++ 52_flashloan_fantom_2/OTHER/16-25/25/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.10",
   networks: {
-    goerli: {
+    fantom: {
       url: process.env.PROVIDER_URL,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
