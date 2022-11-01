b5efb10167a3347508d7c4920ba4515450138e4c
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-11-01 22:45:19 +0300

    contract is finished, deployment files and configurations are finished

--- 57_uniswap_swap/contracts/SingleSwap.sol
+++ 57_uniswap_swap/contracts/SingleSwap.sol
@@ -2,31 +2,33 @@
 pragma solidity =0.7.6;
 pragma abicoder v2; 
 
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns(bool);
+    function balanceOf(address account) external view returns(uint);
+    function approve(address spender, uint256 amount) external returns(bool);
+}
 
 contract SingleSwap {
     ISwapRouter public immutable swapRouter;
     //usdc and link are goerli versions
     address public constant USDC = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
     address public constant LINK = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address public constant USDC_OLD = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant WETH = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    IERC20 public linkToken = IERC20(LINK);
     
+    address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
 
     uint24 public constant poolFee = 3000;
 
-    constructor(ISwapRouter _swapRouter) {
-        swapRouter = _swapRouter;
-    }
-
     function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
-        TransferHelper.safeApprove(LINK, address(swapRouter), amountIn);
+        linkToken.approve(address(swapRouter), amountIn);
         ISwapRouter.ExactInputSingleParams memory params =
             ISwapRouter.ExactInputSingleParams({
                 tokenIn: LINK,
-                tokenOut: USDC,
+                tokenOut: WETH,
                 fee: poolFee,
                 recipient: address(this),
                 deadline: block.timestamp,
@@ -38,12 +40,12 @@ contract SingleSwap {
     }
 
    function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum) external returns (uint256 amountIn) {
-        TransferHelper.safeApprove(LINK, address(swapRouter), amountInMaximum);
+        linkToken.approve(address(swapRouter), amountInMaximum);
 
         ISwapRouter.ExactOutputSingleParams memory params =
             ISwapRouter.ExactOutputSingleParams({
                 tokenIn: LINK,
-                tokenOut: USDC,
+                tokenOut: WETH,
                 fee: poolFee,
                 recipient: address(this),
                 deadline: block.timestamp,
@@ -54,8 +56,8 @@ contract SingleSwap {
         amountIn = swapRouter.exactOutputSingle(params);
 
         if (amountIn < amountInMaximum) {
-            TransferHelper.safeApprove(LINK, address(swapRouter), 0);
-            TransferHelper.safeTransfer(LINK, address(this), amountInMaximum - amountIn);
+            linkToken.approve(address(swapRouter), 0);
+            linkToken.transfer(address(this), amountInMaximum - amountIn);
         }
     }
 }
