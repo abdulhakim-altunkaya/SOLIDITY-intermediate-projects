fd57a52391ee96f374b99a087a19bf624bf07a03
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-11-03 07:27:49 +0300

    contract finished and deployed

--- 57_uniswap_swap/contracts/SingleSwap.sol
+++ 57_uniswap_swap/contracts/SingleSwap.sol
@@ -4,6 +4,7 @@ pragma abicoder v2;
 
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
+
 interface IERC20 {
     function transfer(address recipient, uint amount) external returns(bool);
     function approve(address spender, uint amount) external returns(bool);
@@ -11,23 +12,27 @@ interface IERC20 {
 }
 
 contract SingleSwap {
-    address public constant USDC = 0xe11A86849d99F524cAC3E7A0Ec1241828e332C62;
-    address public constant LINK = "";
-    address public constant WETH = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
-    IERC20 public usdcToken = IERC20(USDC)
+    //GOERLI VERSIONS:
+    address public constant USDC = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
+    address public constant LINK = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    address public constant WETH = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
 
-    address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
+    IERC20 public usdcToken = IERC20(USDC);
+
+
+    ISwapRouter public immutable swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
     uint24 public constant poolFee = 3000;
 
     function swapExactInputSingle(uint amountIn) external returns(uint amountOut) {
-        //we need to approve swaprouter to withdraw the amount from this contract so that it will change it to another currency
+        //we need to approve swapRouter so that it can withdraw amount from this contract. because later it will change it to another currency
+        //instead of "address(swapRouter)" I can use swapRouter I guess
         usdcToken.approve(address(swapRouter), amountIn);
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
             tokenIn: USDC,
             tokenOut: WETH,
             fee: poolFee,
-            recipient: address(this),
+            recipient: 0x547Fdf051df6454322cb7100A4d8edDE8B17a45E,
             deadline: block.timestamp,
             amountIn: amountIn,
             amountOutMinimum: 0,
@@ -42,7 +47,7 @@ contract SingleSwap {
             tokenIn: USDC,
             tokenOut: WETH,
             fee: poolFee,
-            recipient: address(this),
+            recipient: 0x547Fdf051df6454322cb7100A4d8edDE8B17a45E,
             deadline: block.timestamp,
             amountOut: amountOut,
             amountInMaximum: amountInMaximum,
@@ -54,17 +59,17 @@ contract SingleSwap {
             usdcToken.transfer(address(this), amountInMaximum - amountIn);
         }
     }
-
     function getBal18(address _tokenAddress) external view returns(uint) {
         uint balance = IERC20(_tokenAddress).balanceOf(address(this));
         return balance / (10**18);
     }
+
     function getBal6(address _tokenAddress) external view returns(uint) {
         uint balance = IERC20(_tokenAddress).balanceOf(address(this));
         return balance / (10**6);
     }
 
-    function destroyContract() external {
-        selfdestruct(msg.sender);
+    function destroy() external {
+        selfdestruct(payable(0x547Fdf051df6454322cb7100A4d8edDE8B17a45E));
     }
 }
