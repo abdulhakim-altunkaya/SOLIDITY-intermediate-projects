ad04885a26531eae7a201e2c937686abae275dce
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-28 16:27:15 +0300

    web3 package integrated with express and html

---
+++ 65_mittweida_express/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./", "dapp.html"));
+});
+
+
+const server = app.listen(5000);
+const portNumber = server.address().port;
+console.log(`port is open on ${portNumber}`);
