4391a2e69f14ae661e021ef6c66d3bbd43c2125b
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-29 22:20:06 +0300

    pancake files added

---
+++ 66_flash_pancake_read/client/AddressList.js
@@ -0,0 +1,15 @@
+//Enables us to find the right pair contract
+const addressFactory = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+//Gives us the prices
+const addressRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+//What coin we want to interact with BUSD
+const addressFrom = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+//Target coin - WBNB
+const addressTo = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+
+module.exports = {
+    addressFactory,
+    addressRouter,
+    addressFrom,
+    addressTo,
+}
