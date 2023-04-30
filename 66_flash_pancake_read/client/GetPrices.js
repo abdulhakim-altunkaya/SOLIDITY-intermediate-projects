526ca0713587a0a45b23b76d0d619f0fed03db1a
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-30 23:55:39 +0300

    more pancake contracts are read with ethers javascript file

--- 66_flash_pancake_read/client/GetPrices.js
+++ 66_flash_pancake_read/client/GetPrices.js
@@ -4,4 +4,18 @@ const { addressFactory, addressRouter, addressFrom, addressTo } = require("./Add
 const { erc20ABI, factoryABI, routerABI, pairABI } = require("./AbiList");
 
 //Standard Provider
-const provider = new ethers.providers.JsonRpcProvider()
+//If it gives json rpc error, change ethers version to 5.7.2
+const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org");
+let url = "https://bsc-dataseed1.defibit.io";
+let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+//console.log(customHttpProvider);
+
+
+//Connect Factory Contract
+//To read data "provider" is enough. To write data, ie to sign transactions, I need to say "provider.getSigner()"
+const contractFactory = new ethers.Contract(addressFactory, factoryABI, provider);
+//console.log(contractFactory);
+
+//Connect Router Contract
+const contractRouter = new ethers.Contract(addressRouter, routerABI, provider);
+console.log(contractRouter);
